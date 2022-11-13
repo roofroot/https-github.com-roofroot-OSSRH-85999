e8e1742032647eaeddbbad654f62e5c1ff8f2caa
yuxiu <yuxiu5@faw.com.cn>
2022-11-13 16:13:51 +0800

    add some shape and a border

--- widget_lib/src/main/java/cn/open/widget_lib/Shapes.kt
+++ widget_lib/src/main/java/cn/open/widget_lib/Shapes.kt
@@ -134,4 +134,68 @@ class SimpleWaveRectShape(
         return Outline.Generic(path)
     }
 }
+//单个波浪
+class OneWaveShape(
+) : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        var rect = Rect(
+            0f, 0f, size.width, size.height
+        )
+        var path = Path();
+        path.moveTo(rect.left, rect.top)
+        path.quadraticBezierTo(
+            rect.left + size.width / 2, rect.bottom,
+            rect.right, rect.top
+        )
+        path.close()
+        return Outline.Generic(path)
+    }
+}
+
+//半圆
+class HalfCircleShape(
+) : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        var rect = Rect(
+            0f, -size.height, size.width, size.height
+        )
+        var path = Path();
+        path.moveTo(0f,0f)
+        path.addArc(rect,0f,180f)
+        return Outline.Generic(path)
+    }
+}
+
+//tag bg
+class TagBackgroundShape(var arcHeight:Float
+) : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        if(arcHeight>size.height){
+            arcHeight=size.height
+        }
+        var arcTop=size.height-arcHeight*2
+        var rect = Rect(
+            0f, arcTop, size.width, size.height
+        )
+        var path = Path();
+        path.moveTo(size.width,0f)
+        path.lineTo(size.width,arcTop)
+        path.arcTo(rect,0f,180f,false)
+        path.lineTo(0f,0f)
+        path.close()
+        return Outline.Generic(path)
+    }
+}
 
